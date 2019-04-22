b4f2e5b6dda91d4cdf16efdf9e4deb1f5be7e804
wchan7 <48970066+wchan7@users.noreply.github.com>
2019-04-22 16:48:16 -0400

    Add files via upload

---
+++ LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.moody;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        configureMainLogin();
+        configureMainRegister();
+    }
+
+        private void configureMainLogin()
+        {
+            Button mainLogin = (Button) findViewById(R.id.mainLogin);
+            mainLogin.setOnClickListener (new View.OnClickListener()
+            {
+                public void onClick (View view)
+                {
+                    startActivity (new Intent (LoginActivity.this, Login.class));
+                }
+            });
+        }
+
+        private void configureMainRegister()
+        {
+            Button mainRegister = (Button) findViewById(R.id.mainRegister);
+            mainRegister.setOnClickListener (new View.OnClickListener()
+            {
+                public void onClick (View view)
+                {
+                    startActivity (new Intent (LoginActivity.this, Register.class));
+                }
+            });
+        }
+
+}
